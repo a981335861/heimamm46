b93623426e48814f152c458284695f209a91d78d
guangcheng <981335861@qq.com>
2020-02-07 14:22:06 +0800

    整合element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 // 导入路由
 import router from './router/index'
+// 导入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
