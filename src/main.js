2382394ac67a06566b7109cd606be115b246633c
guangcheng <981335861@qq.com>
2020-02-07 12:46:37 +0800

    整合路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入路由
+import router from './router/index'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 挂载到Vue实例上  路由
+  router
 }).$mount('#app')
