dddac48e5aaf30aeb6fbf0531fc897509d5164db
guangcheng <981335861@qq.com>
2020-02-09 11:46:53 +0800

    注册模块 - 表单验证(昵称,密码,手机,邮箱)

--- src/views/login/components/registerDialog.vue
+++ src/views/login/components/registerDialog.vue
@@ -1,35 +1,41 @@
 <template>
-  <el-dialog class="register-dialog" center width="603px" title="用户注册" :visible.sync="dialogFormVisible">
-    <el-form :model="form">
-      <el-form-item label="昵称" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+  <el-dialog
+    class="register-dialog"
+    center
+    width="603px"
+    title="用户注册"
+    :visible.sync="dialogFormVisible"
+  >
+    <el-form status-icon :model="form" :rules="rules" ref="registerForm">
+      <el-form-item label="昵称" prop="username" :label-width="formLabelWidth">
+        <el-input v-model="form.username" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="邮箱" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+      <el-form-item label="邮箱" prop="email" :label-width="formLabelWidth">
+        <el-input v-model="form.email" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="手机" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+      <el-form-item label="手机" prop="phone" :label-width="formLabelWidth">
+        <el-input v-model="form.phone" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="密码" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+      <el-form-item label="密码" prop="password" :label-width="formLabelWidth">
+        <el-input show-password v-model="form.password" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="图形码" :label-width="formLabelWidth">
         <el-row>
-          <el-col :span="17">
+          <el-col :span="16">
             <el-input v-model="form.name" autocomplete="off"></el-input>
           </el-col>
-          <el-col :span="7" class="register-box">
-              <img src="../../../assets/login-code.png" alt="" class="register-code">
+          <el-col :span="7" class="register-box" :offset="1">
+            <img src="../../../assets/login-code.png" alt class="register-code" />
           </el-col>
         </el-row>
       </el-form-item>
       <el-form-item label="验证码" :label-width="formLabelWidth">
-          <el-row>
-          <el-col :span="17">
+        <el-row>
+          <el-col :span="16">
             <el-input v-model="form.name" autocomplete="off"></el-input>
           </el-col>
-          <el-col :span="7">
-              <el-button>点击获取验证码</el-button>
+          <el-col :span="7" :offset="1">
+            <el-button>点击获取验证码</el-button>
           </el-col>
         </el-row>
       </el-form-item>
@@ -42,12 +48,59 @@
 </template>
 
 <script>
+const checkEmail = ((rule, value, callback) => {
+  const reg =/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
+  if (reg.test(value)==true) {
+    callback()
+  } else {
+    callback(new Error("邮箱的格式不对"))
+  }
+});
+const checkPhone = ((rule, value, callback) => {
+  const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+  if (reg.test(value)==true) {
+    callback()
+  } else {
+    callback(new Error("邮箱的格式不对"))
+  }
+});
 export default {
   data() {
     return {
       dialogFormVisible: false,
       form: {
-        name: ""
+        name: "",
+        // 昵称
+        username: "",
+        // 密码
+        password: "",
+        // 邮箱
+        email: "",
+        // 手机
+        phone: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 6, max: 12, message: "密码的长度为6-12位", trigger: "change" }
+        ],
+        password: [
+          { required: true, message: "验证码不能为空", trigger: "blur" },
+          {
+            min: 6,
+            max: 12,
+            message: "验证码的长度为6-12位",
+            trigger: "change"
+          }
+        ],
+        email: [
+          { required: true, message: "邮箱不能为空", trigger: "blur" },
+          { validator: checkEmail, trigger: "blur" }
+        ],
+        phone: [
+          { required: true, message: "邮箱不能为空", trigger: "blur" },
+          { validator: checkPhone, trigger: "blur" }
+        ]
       },
       // 左侧文本的间隙
       formLabelWidth: "62px"
@@ -57,20 +110,19 @@ export default {
 </script>
 
 <style lang="less">
-.register-dialog{
-    .el-dialog__header{
-        background: linear-gradient(to right,#0dbfef,#1792f8);
-    }
-    .el-dialog__title{
-        color: #fff;
-    }
-    .register-box{
-        height: 40px;
-    }
-    .register-code{
-        height: 40px;
-        width: 100%;
-    }
-
+.register-dialog {
+  .el-dialog__header {
+    background: linear-gradient(to right, #0dbfef, #1792f8);
+  }
+  .el-dialog__title {
+    color: #fff;
+  }
+  .register-box {
+    height: 40px;
+  }
+  .register-code {
+    height: 40px;
+    width: 100%;
+  }
 }
 </style>
