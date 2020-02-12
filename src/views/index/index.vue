fbd0e960dd06d60a8bf2fbf4b9e1142e476ea3d3
guangcheng <981335861@qq.com>
2020-02-12 20:59:48 +0800

    完成首页模块 - 左侧导航栏&退出登录 完成登录校验模块 - token的非空判断&真假判断&判断前置&路由白名单

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,42 +1,95 @@
 <template>
   <el-container class="my-container">
     <el-header class="my-header">
-        <div class="left">
-            <i class="el-icon-s-fold"></i>
-            <img src="../../assets/index_logo.png" alt="">
-            <span>黑马面面</span>
-        </div>
-        <div class="right">
-            <img :src="userIcon" alt="">
-            <span class="name">{{username}},您好</span>
-            <el-button type="primary">退出</el-button>
-        </div>
+      <div class="left">
+        <i @click="isCollapse = !isCollapse" class="el-icon-s-fold"></i>
+        <img src="../../assets/index_logo.png" alt />
+        <span>黑马面面</span>
+      </div>
+      <div class="right">
+        <img :src="userIcon" alt />
+        <span class="name">{{username}},您好</span>
+        <el-button type="primary" @click="logout">退出</el-button>
+      </div>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="my-aside">Aside</el-aside>
-      <el-main class="my-main">Main</el-main>
+      <!-- 左侧栏 -->
+      <el-aside width="auto" class="my-aside">
+        <el-menu
+          router
+          :collapse="isCollapse"
+          :default-active="$route.path"
+          class="el-menu-vertical-demo"
+        >
+          <el-menu-item index="/index/chart">
+            <i class="el-icon-pie-chart"></i>
+            <span slot="title">数据概览</span>
+          </el-menu-item>
+          <el-menu-item index="/index/user">
+            <i class="el-icon-user"></i>
+            <span slot="title">用户列表</span>
+          </el-menu-item>
+          <el-menu-item index="/index/question">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">题库列表</span>
+          </el-menu-item>
+          <el-menu-item index="/index/enterprise">
+            <i class="el-icon-office-building"></i>
+            <span slot="title">企业列表</span>
+          </el-menu-item>
+          <el-menu-item index="/index/subject">
+            <i class="el-icon-notebook-2"></i>
+            <span slot="title">学科列表</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main class="my-main">
+        <!-- 路由出口 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-import {info} from '@/api/index.js'
+// 导入接口
+import { logout } from "@/api/index.js";
+// 导入 token函数
+import { removeToken } from "@/utils/token.js";
 export default {
-    name:'index',
-    data() {
-        return {
-            // 用户名
-            username:"",
-            // 用户头像
-            userIcon:"",
-        }
-    },
-    created(){
-        info().then(res=>{
-            this.username = res.data.data.username;
-            this.userIcon = process.env.VUE_APP_URL+'/'+res.data.data.avatar;
+  name: "index",
+  data() {
+    return {
+      // 用户名
+      username: "",
+      // 用户头像
+      userIcon: "",
+      // 是否折叠
+      isCollapse: false
+    };
+  },
+  methods: {
+    logout() {
+      this.$confirm("你确定要离开我们网站", "友情提示", {
+        confirmButtonText: "狠心离开",
+        cancelButtonText: "继续看看",
+        // type: 'error'
+        type: "success"
+      })
+        .then(() => {
+          // 点击确定
+          logout().then(res => {
+            if (res.data.code === 200) {
+              removeToken();
+              this.$router.push("/login");
+            }
+          });
         })
+        .catch(() => {
+          // 点击取消
+        });
     }
+  }
 };
 </script>
 
@@ -75,11 +128,12 @@ export default {
       }
     }
   }
-  .my-aside {
-    background: yellowgreen;
-  }
   .my-main {
     background: #0094ff;
+  }
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
   }
 }
 </style>
